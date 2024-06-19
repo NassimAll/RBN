0c9581d1be2c0f8f95ac65078dfe4153dfa776b5
Nassim Allagui <allagui45@gmail.com>
2024-06-19 21:19:06 +0200

    adding maschera to inital condition generator

--- generatore_initcond.py
+++ generatore_initcond.py
@@ -6,20 +6,37 @@ output_dir = os.getcwd() + "/output/"
 
 #Leggiamo i parametri in input dal file
 def read_param():
-    parametri = {}
     with open(input_file, 'r') as file:
-        for line in file:
-            if line.strip():
-                key, value = line.split(':')
-                parametri[key.strip()] = value.strip()
-    return parametri
+        # Legge tutte le righe del file
+        lines = file.readlines()
 
-def gen_condizioni_iniziali(n_genes, n_cond, bias, seme):
+    # Estrae i valori delle variabili
+    n_genes = int(lines[0].split(":")[1].strip())
+    n_cond = int(lines[1].split(":")[1].strip())
+    bias = float(lines[2].split(":")[1].strip())
+    seme = int(lines[3].split(":")[1].strip())
+    
+    # Legge la maschera e separa il primo 1 dalla lista rimanente
+    maschera = list(map(int, lines[4].split(":")[1].strip().split()))
+    flag = maschera[0]
+    mask = list(map(int, lines[5].split()))
+
+    return n_genes, n_cond, bias, seme, flag, mask
+
+def gen_condizioni_iniziali(n_genes, n_cond, bias, seme, maschera):
     random.seed(seme)
     condizioni = []
     
     for _ in range(n_cond):
-        condizione = [random.choices([0, 1], [(1.0 - bias), bias])[0] for _ in range(n_genes)]
+        condizione = []
+        for i in range(n_genes):
+            if maschera[i] == 0:
+                val = 0
+            elif maschera[i] == 1:
+                val = 1
+            elif maschera[i] == 2:
+                val = random.choices([0, 1], [(1.0 - bias), bias])[0]
+            condizione.append(val)
         condizioni.append(condizione)
     
     return condizioni
@@ -34,18 +51,11 @@ def write_out(condizioni, out_file, n_genes, n_cond):
 if __name__ == '__main__':
     
     # Lettura dei parametri dal file
-    parametri_iniziali = read_param()
-
-    # Conversione dei parametri letti dal file al tipo corretto
-    n_genes = int(parametri_iniziali['n_genes'])
-    n_cond = int(parametri_iniziali['n_cond'])
-    bias = float(parametri_iniziali['bias'])
-    seme = int(parametri_iniziali['seme'])
-    maschera = int(parametri_iniziali['maschera'])
+    n_genes, n_cond, bias, seme, flag, mask = read_param()
 
 
     # Generazione delle condizioni iniziali
-    condizioni_iniziali = gen_condizioni_iniziali(n_genes, n_cond, bias, seme)
+    condizioni_iniziali = gen_condizioni_iniziali(n_genes, n_cond, bias, seme, mask)
 
     # Scrittura delle condizioni su file
     write_out(condizioni_iniziali, (output_dir+'condizioni_iniziali.txt'), n_genes, n_cond)
