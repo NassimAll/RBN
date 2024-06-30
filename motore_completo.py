8e0bc1da14eea71b24121eee4f69e7cf9972a9b2
Nassim Allagui <allagui45@gmail.com>
2024-06-30 15:41:55 +0200

    Finita modalità 1 e 2, to do mode 3

--- motore_completo.py
+++ motore_completo.py
@@ -9,10 +9,14 @@ input_condInit =  os.getcwd() + "/output/condizioni_iniziali.txt"
 output_dir = os.getcwd() + "/output/"
 
 
-def load_network_from_json():
+def load_rete():
     with open(input_grafo, 'r') as f:
         network = json.load(f)
     return network
+
+#Costruisco il binario dagli input e lo trasformo nell'indice che mi rappresenta il suo out
+# inputs --> 0 1 --> 01 --> index = 1 --> outputs[1]
+# inputs --> 1 1 --> 11 --> index = 3 --> outputs[3]
 
 def boolean_function_output(inputs, outputs):
     index = int(''.join(map(str, inputs)), 2)
@@ -23,20 +27,20 @@ def simulate_rbn(network, initial_conditions, steps):
     results = []
     
     for initial in initial_conditions:
-        state = np.array(initial)
-        trajectory = [state.copy()]
+        state = np.array(initial) #Carico lo stato iniziale
+        traiettoria = [state.copy()] #inizializzo la traiettoria al primo stato
         
         for _ in range(steps):
-            new_state = state.copy()
-            for node, info in network.items():
+            new_state = state.copy() # stato successivo
+            for node, info in network.items(): # Calcolo lo stato per ogni singolo nodo
                 node = int(node)
-                ingressi = info['ingressi']
-                uscite = info['uscite']
-                inputs = [state[i] for i in ingressi]
-                new_state[node] = boolean_function_output(inputs, uscite)
-            state = new_state
-            trajectory.append(state.copy())
-        results.append(trajectory)
+                ingressi = info['ingressi'] # nodi in ingresso
+                uscite = info['uscite'] # tabella delle uscite
+                inputs = [state[i] for i in ingressi] # carico il valore degli ingressi nel nodo, in ordine
+                new_state[node] = boolean_function_output(inputs, uscite) # funzione per calcolare il valore dell'uscita
+            state = new_state # salvo il nuovo stato 
+            traiettoria.append(state.copy())
+        results.append(traiettoria)
     return results
 
 
@@ -60,6 +64,30 @@ def read_init_conditions():
     
     return initial_conditions, n_cond
 
+def print_result_mode_1(results, initial_conditions):
+     # Print the results
+    with open(os.path.join(output_dir, "output_motore.txt"), 'w') as file:
+        for i, result in enumerate(results):
+            file.write(f"Initial condition {i+1}: {initial_conditions[i]}\n")
+            file.write(f" Last step : {result[-1]}\n")
+            #print()
+
+def print_result_mode_2(results, initial_conditions):
+     # Print the results
+    with open(os.path.join(output_dir, "output_motore.txt"), 'w') as file:
+        for i, result in enumerate(results):
+            #print(f"Initial condition {i+1}: {initial_conditions[i]}")
+            file.write(f"Initial condition {i+1}: {initial_conditions[i]}\n")
+            for step, state in enumerate(result):
+                #print(f" Step {step}: {state}")
+                file.write(f" Step {step}: {state}\n")
+            #print()
+
+
+def print_result_mode_3(results, initial_conditions):
+    #TO DO
+    return
+
 if __name__ == "__main__":
 
     # Carichiamo i parametri del motore
@@ -72,20 +100,27 @@ if __name__ == "__main__":
     fin_max = int(parametri['finmax'])
     
     # Carichiamo la rete e la sequenza di ocndizioni iniziali
-    network = load_network_from_json()
+    network = load_rete()
     initial_conditions, n_cond = read_init_conditions()
 
-    # Simulate the network for each initial condition for a given number of steps
-    steps = 10
-    results = simulate_rbn(network, initial_conditions, steps)
+    # Eseguo la simulazione
+    #steps = 10
+    results = simulate_rbn(network, initial_conditions, n_steps)
     
-    # Print the results
-    with open(os.path.join(output_dir, "output_motore.txt"), 'w') as file:
-        for i, result in enumerate(results):
-            print(f"Initial condition {i+1}: {initial_conditions[i]}")
-            file.write(f"Initial condition {i+1}: {initial_conditions[i]}\n")
-            for step, state in enumerate(result):
-                print(f" Step {step}: {state}")
-                file.write(f" Step {step}: {state}\n")
-            print()
+    if mode == 1:
+        print_result_mode_1(results, initial_conditions)
+    elif mode == 2:
+        print_result_mode_2(results, initial_conditions)
+    elif mode == 3:
+        print_result_mode_3(results)
+
+    # # Print the results
+    # with open(os.path.join(output_dir, "output_motore.txt"), 'w') as file:
+    #     for i, result in enumerate(results):
+    #         print(f"Initial condition {i+1}: {initial_conditions[i]}")
+    #         file.write(f"Initial condition {i+1}: {initial_conditions[i]}\n")
+    #         for step, state in enumerate(result):
+    #             print(f" Step {step}: {state}")
+    #             file.write(f" Step {step}: {state}\n")
+    #         print()
 
